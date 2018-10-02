df0b87d7471c0f6dde89e1414c2124e1f616feab
Yashkaran <ykk0989@gmail.com>
2018-10-02 16:14:44 -0700

    Initial Commit

---
+++ binningtax/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BinningtaxConfig(AppConfig):
+    name = 'binningtax'
---
+++ binningtax/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'binningtax/home.html')
+
+
+def tax(request):
+    return render(request, 'binningtax/tax.html')
+    # return HttpResponse("Hello from tax page")
