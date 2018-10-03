560bbeaae93c06e4d7bef37f888ee72fb39967ee
ysingh89 <ykk0989@gmail.com>
2018-10-02 21:31:39 -0700

    fixed url

--- binningtax/urls.py
+++ binningtax/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home),
     path('home/', views.home, name='home'),
     path('tax/', views.tax, name='tax'),
 ]
