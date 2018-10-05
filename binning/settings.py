0e656426fb1f650259e600e41625c131e6cb4faf
ysingh89 <ykk0989@gmail.com>
2018-10-05 16:38:26 -0700

    fixed env variables

--- binning/settings.py
+++ binning/settings.py
@@ -131,3 +131,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_resources')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
