b328c8390c0c0f77935e033e4ed33752578bb7c1
ysingh89 <ykk0989@gmail.com>
2018-10-06 16:43:26 -0700

    improved urls

--- binningtax/urls.py
+++ binningtax/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('home/', views.home, name='home'),
-    path('tax/', views.tax, name='tax'),
-    path('immigration/', views.immigration, name='immigration'),
-    path('indian/', views.indian, name='indian'),
-    path('contact', views.contact, name='contact'),
+    path('tax-services/', views.tax, name='tax-services'),
+    path('immigration-help/', views.immigration, name='immigration-help'),
+    path('indian-services/', views.indian, name='indian-services'),
+    path('contact-us/', views.contact, name='contact-us'),
 ]
