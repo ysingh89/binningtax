c20dbcc50259df00814f85d2aafa60d671acf81b
ysingh89 <ykk0989@gmail.com>
2018-10-06 17:38:18 -0700

    modified allowed hosts

--- binning/settings.py
+++ binning/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
+ALLOWED_HOSTS = [os.environ['HOST_NAME']]
 
 
 # Application definition
